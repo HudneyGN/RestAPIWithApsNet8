d5b0a26911744335dd85560fd05cde9ca7739743
Hudney Gomes Nunes <hudneygn@gmail.com>
2025-04-25 16:17:11 -0300

    Implementing calculator

--- RestAPIWithApsNet8/obj/Debug/net8.0/RestAPIWithApsNet8.AssemblyInfo.cs
+++ RestAPIWithApsNet8/obj/Debug/net8.0/RestAPIWithApsNet8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestAPIWithApsNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b4386ba6fce14a94efdac1cdbfba32da682cc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestAPIWithApsNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestAPIWithApsNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
